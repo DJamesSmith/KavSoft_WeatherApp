cd18eb1011438c1cb58daa9b010a1bd991e48a10
Dion James Smith <dion.smith070@gmail.com>
2023-03-18 13:47:15 +0530

    Initial Commit

---
+++ KavSoftWeatherScrolling/KavSoftWeatherScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  KavSoftWeatherScrollingApp.swift
+//  KavSoftWeatherScrolling
+//
+//  Created by Dion James Smith on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct KavSoftWeatherScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
